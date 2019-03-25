0a0a9e8849fcc98713b005c3b2b6bec40ff6db98
avogt <avogt@bancoestado.cl>
2019-03-24 21:40:19 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,4 +4,23 @@
 
 process.env.PORT = process.env.PORT || 3000;
 
+//===========================================
+//     Enviroment
+//===========================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//===========================================
+//     Enviroment
+//===========================================
+let urlDB;
+
+if( process.env.NODE_ENV === 'dev' ){
+    urlDB= 'mongodb://localhost:27017/cafe';
+}else{
+    ulrDB= 'mongodb+srv://avogt:I6f5zuH1Kwy6I3mx@cluster0-9wtf1.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
+
 
--- server/server.js
+++ server/server.js
@@ -1,48 +1,26 @@
 require('./config/config');
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
 
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use( require('./routes/usuario'));
  
 // parse application/json
 app.use(bodyParser.json())
 
- 
-app.get('/usuario', function (req, res) {
-  res.json('get usuario')
-});
+  mongoose.connect(process.env.URLDB,
+       {useNewUrlParser: true,useCreateIndex: true}, 
+       (err,res)=>{
 
-app.post('/usuario', function (req, res) {
+      if(err) throw err;
 
-    let body = req.body;
-
-    if( body.nombre === undefined )
-
-        res.status(400).json({
-            ok:false,
-            mensaje:"El nombre de usuario es necesario"
-        });
-
-    res.json({
-        persona:body
-    })
-  });
-
-  app.put('/usuario/:id', function (req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    })
-  });
-
-  app.delete('/usuario/:id', function (req, res) {
-    res.json('delete usuario')
+      console.log("Base de datos ONLINE!!!");
   });
 
 
