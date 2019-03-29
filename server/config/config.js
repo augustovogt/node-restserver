4dc9d948de23c3be4fcb571521474190a9165f10
avogt <avogt@bancoestado.cl>
2019-03-29 17:53:06 -0300

    logea conexion mongodb

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,9 @@ if( process.env.NODE_ENV === 'dev' ){
 }else{
     ulrDB= 'mongodb+srv://avogt:I6f5zuH1Kwy6I3mx@cluster0-9wtf1.mongodb.net/cafe';
 }
-
+console.log("===>process.env.NODE_ENV:",process.env.NODE_ENV);
 process.env.URLDB = urlDB;
+
+console.log("===>process.env.URLDB:",process.env.URLDB);
 
 
